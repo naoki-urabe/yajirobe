98b15a879e633b5e52864e7407f55ab196a94acc
naoki-urabe <na0k1ura6e@gmail.com>
2021-10-02 12:28:17 +0000

    docker image pruneのprompt skip

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ USER_HOST=`echo "$USER@$HOST"`
 SYNC_PATH=`echo "$USER_HOST:/home/$USER/app/yajirobe"`
 SSH_PORT="ssh -p $HOST_PORT"
 rsync -e "$SSH_PORT" -acvz  --delete ./ $SYNC_PATH
-ssh $USER_HOST -p $HOST_PORT "cd app/yajirobe;/usr/bin/docker-compose down -v; docker image prune;docker pull ghcr.io/naoki-urabe/yajirobe/api:prod;docker pull ghcr.io/naoki-urabe/yajirobe/front:prod;docker pull ghcr.io/naoki-urabe/yajirobe/task:prod;/usr/bin/docker-compose -f docker-compose.prod.yml up -d"
+ssh $USER_HOST -p $HOST_PORT "cd app/yajirobe;/usr/bin/docker-compose down -v; docker image prune -f;docker pull ghcr.io/naoki-urabe/yajirobe/api:prod;docker pull ghcr.io/naoki-urabe/yajirobe/front:prod;docker pull ghcr.io/naoki-urabe/yajirobe/task:prod;/usr/bin/docker-compose -f docker-compose.prod.yml up -d"
