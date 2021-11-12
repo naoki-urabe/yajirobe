710789ddbb1cf79a1511eb414fd37636888983f0
naoki-urabe <na0k1ura6e@gmail.com>
2021-11-13 07:55:05 +0900

    登録時に値のvalidationをかけるように変更

--- front/server.js
+++ front/server.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
-app = express();
+const app = express();
 app.use(serveStatic(__dirname + '/dist'));
 var host = process.env.VUE_APP_HOST
 var port = process.env.VUE_APP_FRONT_PORT;
